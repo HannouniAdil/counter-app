b78a4332c7f67e17e5e0574f0ce21261cf0ce018
adilhannouni <adilhannouni@gmail.com>
2019-05-27 11:44:51 -0700

    Remove State and Let Props handle data manupilation

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,45 +1,19 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-    imageUrl: "https://picsum.photos/200",
-    tags: ["item1", "item2", "item3"],
-    products: ["prd1", "prod2", "prod3"]
-  };
-
   styles = {
     fontSize: 14,
     fontWeight: "bold"
   };
 
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
   render() {
     let classes = this.getBadgeClasses();
-    console.log(this.props);
     return (
-      <React.Fragment>
-        <img src={this.state.imageUrl} />
+      <div>
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrement(this.state.products[2])}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -50,21 +24,18 @@ class Counter extends Component {
         >
           Delete
         </button>
-
-        {this.state.tags.length === 0 && "Please add new tags"}
-        {this.renderTags()}
-      </React.Fragment>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     const x = <h1>Zero</h1>;
     return value === 0 ? x : value;
   }
